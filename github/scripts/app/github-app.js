4260986f039a48855ed93bdb69953461ba4d4e1d
AlsoCalledChris <alsocalledchris@gmail.com>
2017-02-12 18:06:40 +0000

    Remove un-needed files

--- github/scripts/app/github-app.js
+++
@@ -1,8 +0,0 @@
-var repoSearchMopdule = angular.module("Github", ["ui.router", "angularSpinner"])
-    .config(["$stateProvider", "$urlRouterProvider", function ($stateProvider, $urlRouterProvider) { return new App.Routes().configue($stateProvider, $urlRouterProvider); }])
-    .service("RepoSearchService", ["$http", "$q", function ($http, $q) { return new App.RepoSearch.Services.RepoSearchService($http, $q); }])
-    .service("RepoIssuesService", ["$http", "$q", function ($http, $q) { return new App.RepoIssues.Services.RepoIssuesService($http, $q); }])
-    .controller("GithubController", ["$state", "usSpinnerService", function ($state, spinner) { return new App.Controllers.GithubController($state, spinner); }])
-    .controller("RepoSearchController", ["RepoSearchService", "$state", "usSpinnerService", function (repoSearchService, $state, spinner) { return new App.RepoSearch.Controllers.RepoSearchController(repoSearchService, $state, spinner); }])
-    .controller("RepoIssuesController", ["RepoIssuesService", "$state", "usSpinnerService", function (repoIssuesService, $state, spinner) { return new App.RepoIssues.Controllers.RepoIssuesController(repoIssuesService, $state, spinner); }])
-    .directive("tooltipHoverDirective", ["$timeout", function ($timeout) { return App.RepoSearch.Directives.ToolTipHover.factory($timeout); }]);
