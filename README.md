ddf84e3a9bb67fe242518206ebf225d70fb58910
AlsoCalledChris <alsocalledchris@gmail.com>
2017-02-12 18:03:48 +0000

    Clean up build tasks

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Github-search-example-with-Angular-and-Typescript
 Example use of Typescript with AngularJS hitting Github API
 
-This project is an example of how to use Typescript with AngularJS. It uses Gulp and Bower for mini-fication and dependencies and is hosted via Node.
+This project is an example of how to use Typescript with AngularJS. It uses Gulp and Bower for build tasks and JS dependencies and is hosted via Node (npm start).
 
-To run get the latest from Github and run the build task.
+To run get the latest from Github and run the build task. Creates a release folder as well with mini-fied CSS and JS. Watch task is used to generate the JS and CSS files from Typescript and SASS files.
--- github/gulpfile.js
+++ github/gulpfile.js
@@ -1,5 +1,4 @@
 ﻿var gulp = require("gulp"),
-rimraf = require("rimraf"),
 concat = require("gulp-concat"),
 cssmin = require("gulp-cssmin"),
 uglify = require("gulp-uglify"),
@@ -12,43 +11,34 @@ lec = require("gulp-line-ending-corrector"),
 runSequence = require('run-sequence');
 
 var paths = {
-    webroot: "./release/",
+    releaseFolder: "./release/",
     scriptslib: "scripts/lib/",
     css: "./css/**/*.css",
-    app: "./scripts/app/",
-    tsscripts: ["scripts/app/**/*.js", "scripts/app/**/*.ts", "scripts/app/**/*.map"],
-    html: ["scripts/app/**/*.html"],
+    appJsFiles: "./scripts/app/",
+    html: ["app/**/*.html"],
     jslibraryincludes: ["scripts/lib/" + "jquery/dist/jquery.js", "scripts/lib/" + "bootstrap/dist/js/bootstrap.js",
            "scripts/lib/" + "angular/angular.js", "scripts/lib/" + "angular-ui-router/release/angular-ui-router.js",
             "scripts/lib/" + "spin.js/spin.js", "scripts/lib/" + "angular-spinner/angular-spinner.js"],
     csslibraryinclude: ["scripts/lib/" + "bootstrap/dist/css/bootstrap.css"]
 };
 
-paths.concatJsDest = paths.webroot + "scripts/site.min.js";
-paths.concatCssDest = paths.webroot + "css/site.min.css";
+paths.concatJsDest = paths.releaseFolder + "scripts/site.min.js";
+paths.concatCssDest = paths.releaseFolder + "css/site.min.css";
 
-gulp.task("clean:app", function () {
-    rimraf("scripts/app");
+gulp.task("clean:app", function (cb) {
+     return del(["scripts/app"]);
 });
 
-gulp.task("clean:css", function () {
-    rimraf("css");
+gulp.task("clean:css", function (cb) {
+     return del(["css"]);
 });
 
-gulp.task("clean:release", function () {
-    rimraf("release");
+gulp.task("clean:release", function (cb) {
+    return del(["release"]);
 });
 
 gulp.task('cleanscriptsfolder', function () {
     return del(["release/scripts/**/*"]);
-});
-
-gulp.task('copyscriptsfolder', function () {
-    gulp.src(paths.tsscripts).pipe(gulp.dest("release/scripts/app"));
-    gulp.src(paths.html).pipe(gulp.dest("release/scripts/app"));
-    gulp.src(paths.jslibraryincludes).pipe(gulp.dest("release/scripts"));
-    gulp.src(paths.csslibraryinclude).pipe(gulp.dest("release/css"));
-    gulp.src(paths.css).pipe(gulp.dest("release/css"));
 });
 
 gulp.task("min:js", function () {
@@ -59,11 +49,11 @@ gulp.task("min:js", function () {
             paths.scriptslib + "angular-ui-router/release/angular-ui-router.js",
             paths.scriptslib + "spin.js/spin.js",
             paths.scriptslib + "angular-spinner/angular-spinner.js",
-            paths.app + "repo-issues/*.js",
-            paths.app + "repo-issues/model/*.js",
-            paths.app + "repo-search/*.js",
-            paths.app + "repo-search/model/*.js",
-            paths.app + "*.js"])
+            paths.appJsFiles + "repo-issues/*.js",
+            paths.appJsFiles + "repo-issues/model/*.js",
+            paths.appJsFiles + "repo-search/*.js",
+            paths.appJsFiles + "repo-search/model/*.js",
+            paths.appJsFiles + "*.js"])
           .pipe(concat(paths.concatJsDest))
           .pipe(uglify({ mangle: false }))
           .pipe(gulp.dest("."));
@@ -78,11 +68,11 @@ gulp.task("min:css", function () {
 
 gulp.task("copyfonts", function () {
     gulp.src(paths.scriptslib + "bootstrap/dist/fonts/**/*.{ttf,woff,woff2,eof,svg}")
-    .pipe(gulp.dest(paths.webroot + "fonts"));
+    .pipe(gulp.dest(paths.releaseFolder + "fonts"));
 });
 
 gulp.task("copyhtml", function () {
-    gulp.src(paths.html).pipe(gulp.dest('release/scripts/app'));
+    gulp.src(paths.html).pipe(gulp.dest('release/app'));
 });
 
 gulp.task('min:htmlreplace', function() {
@@ -103,7 +93,6 @@ gulp.task("sass", function () {
 });
 
 var tsProject = ts.createProject("tsconfig.json");
-
 gulp.task("typescript", function () {
     var tsResult = tsProject.src()
         .pipe(tsProject());
